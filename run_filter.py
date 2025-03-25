ae748ae8c0cfb83e6a59b32db08fab0fc50e79f6
Skauvold <jacob.skauvold@nr.no>
2025-03-25 21:52:40 +0100

    Particle filter implementation

---
+++ run_filter.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utils import (
+    GenExp,
+    GaussianProcess,
+    TopBaseProcessPair,
+    ZonalField,
+    ParticleFilter,
+)
+
+domain = {"xmin": 0, "xmax": 1, "n": 40}
+
+# Define the covariance function
+cov_func_base = GenExp(
+    {"standard_deviation": 0.15, "correlation_range": 0.30, "exponent": 1.9}
+)
+
+cov_func_thickness = GenExp(
+    {"standard_deviation": 0.10, "correlation_range": 0.40, "exponent": 1.8}
+)
+
+
+# Define the mean functions
+def mean_func_base(x):
+    a = 0.67
+    b = 0.5
+    return a + b * x
+
+
+def mean_func_thickness(x):
+    return np.full_like(x, 0.5)
+
+
+# Create process pair
+gp_base = GaussianProcess(cov_func_base, mean_func_base, domain)
+gp_thickness = GaussianProcess(cov_func_thickness, mean_func_thickness, domain)
+tbpp = TopBaseProcessPair(gp_base, gp_thickness)
+
+# Create GR field
+gr_field = ZonalField([0.0, 0.4, 0.6, 1.0], [0.9, 0.33, 0.67, 0.1])
+
+# Prep well data
+well_data = {
+    "x": [0.15, 0.25],
+    "z": [0.5, 0.4],
+    "gr": [0.8, 0.2],
+}
+
+# Set up PF
+pf = ParticleFilter(
+    n_particles=100,
+    tbpp=tbpp,
+    zf=gr_field,
+    noise_params={"sd_gr": 0.10},
+    well_data=well_data,
+    domain=domain,
+)
--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ class CovarianceFunction:
         for i in range(n):
             K[i] = self.cov(x, xs[i])
         return K
-    
+
     def cross_cov_many_to_many(self, xs, ys):
         n = len(xs)
         m = len(ys)
@@ -63,6 +63,11 @@ class GaussianProcess:
 
     def draw_full(self):
         return np.random.multivariate_normal(self.mean_vec, self.K)
+
+    def draw_single(self, x: float) -> float:
+        var = self.cov_func.cov_matrix([x])[0, 0]
+        mean = self.mean_func([x])[0]
+        return np.random.normal(mean, np.sqrt(var))
 
     def draw_conditional(self, cond_indices: list, cond_vals: list):
         K = self.K
@@ -106,9 +111,11 @@ class GaussianProcess:
         mu = mu2 + np.dot(K21, np.linalg.solve(K11, np.array(cond_vals) - mu1))
         cov = K22 - np.dot(K21, np.linalg.solve(K11, K12))
         return np.random.normal(mu, cov)
-    
+
     # - draw conditional value at an x-value given a list of x-values and their z-values
-    def draw_conditional_points(self, xvals_query: list, xvals_cond: list, zvals_cond: list):
+    def draw_conditional_points(
+        self, xvals_query: list, xvals_cond: list, zvals_cond: list
+    ):
         K = self.K
         K11 = self.cov_func.cov_matrix(xvals_cond)
         K12 = self.cov_func.cross_cov_many_to_many(xvals_query, xvals_cond)
@@ -134,12 +141,18 @@ class TopBaseProcessPair:
         top_sample = base_sample - thickness_sample
         return {"top": top_sample, "base": base_sample}
 
+    def draw_single(self, x: float) -> dict:
+        base_sample = self.base_gp.draw_single(x)
+        thickness_sample = self.thickness_gp.draw_single(x)
+        top_sample = base_sample - thickness_sample
+        return {"top": top_sample, "base": base_sample}
+
     def mean_top(self):
         return self.base_gp.mean() - self.thickness_gp.mean()
 
     def mean_base(self):
         return self.base_gp.mean()
-    
+
     def interpolate_sample(self, xs: list, sample: dict):
         zs_base = sample["base"]
         zs_top = sample["top"]
@@ -178,16 +191,23 @@ class TopBaseProcessPair:
         top_sample = base_sample - thickness_sample
         return {"top": top_sample, "base": base_sample}
 
-    # TODO:
-    # - draw conditional value at an x-value given a list of x-values and their z-values
-    def draw_conditional_points(self, xvals_query: list, xvals_cond: list, zvals_cond: dict):
+    # draw conditional value at an x-value given a list of x-values and their z-values
+    def draw_conditional_points(
+        self, xvals_query: list, xvals_cond: list, zvals_cond: dict
+    ):
         zvals_cond_base = zvals_cond["base"]
         zvals_cond_top = zvals_cond["top"]
-        thickness_cond_as_list = [cv_base - cv_top for cv_base, cv_top in zip(zvals_cond_base, zvals_cond_top)]
+        thickness_cond_as_list = [
+            cv_base - cv_top for cv_base, cv_top in zip(zvals_cond_base, zvals_cond_top)
+        ]
         base_cond_as_list = [cv_base for cv_base in zvals_cond_base]
 
-        base_sample = self.base_gp.draw_conditional_points(xvals_query, xvals_cond, base_cond_as_list)
-        thickness_sample = self.thickness_gp.draw_conditional_points(xvals_query, xvals_cond, thickness_cond_as_list)
+        base_sample = self.base_gp.draw_conditional_points(
+            xvals_query, xvals_cond, base_cond_as_list
+        )
+        thickness_sample = self.thickness_gp.draw_conditional_points(
+            xvals_query, xvals_cond, thickness_cond_as_list
+        )
 
         top_sample = base_sample - thickness_sample
         return {"top": top_sample, "base": base_sample}
@@ -205,11 +225,112 @@ class ZonalField:
             return signal + noise
         else:
             return signal
-    
-    def triplet_trend_prediction(self, z_well: float, z_top: float, z_base: float) -> float:
+
+    def triplet_trend_prediction(
+        self, z_well: float, z_top: float, z_base: float
+    ) -> float:
         rel_thickness = (z_well - z_top) / (z_base - z_top)
         return np.interp(rel_thickness, self.trend_zrel, self.trend_vals)
-    
-    def triplet_likelihood(self, z_well: float, z_top: float, z_base: float, observed_value: float, standard_deviation: float) -> float:
+
+    def triplet_likelihood(
+        self,
+        z_well: float,
+        z_top: float,
+        z_base: float,
+        observed_value: float,
+        standard_deviation: float,
+    ) -> float:
         predicted_value = self.triplet_trend_prediction(z_well, z_top, z_base)
-        return stats.norm.pdf(observed_value, loc=predicted_value, scale=standard_deviation)
+        return stats.norm.pdf(
+            observed_value, loc=predicted_value, scale=standard_deviation
+        )
+
+
+class Particle:
+    def __init__(self, tb_sample: dict, xvals: list, weight: float):
+        self.x = xvals
+        self.z_top = tb_sample["top"]
+        self.z_base = tb_sample["base"]
+        self.weight = weight
+
+    def tb(self):
+        return {"top": self.z_top, "base": self.z_base}
+
+    def extend(self, tb_sample: dict, xs, overlap: int = 1):
+        self.x.extend(xs[overlap:])
+        self.z_top.extend(tb_sample["top"][overlap:])
+        self.z_base.extend(tb_sample["base"][overlap:])
+
+
+class ParticleFilter:
+    def __init__(
+        self,
+        n_particles,
+        tbpp: TopBaseProcessPair,
+        zf: ZonalField,
+        noise_params: dict,
+        well_data: dict,
+        domain: dict,
+    ):
+        self.n_particles = n_particles
+        self.tbpp = tbpp
+        self.gr_trend = zf
+        self.noise_params = noise_params
+
+        self.x_obs = well_data["x"]
+        self.z_obs = well_data["z"]
+        self.gr_obs = well_data["gr"]
+
+        self.domain = domain
+        self.particles = self.init_particles()
+
+    def init_particles(self):
+        particles = []
+
+        x0 = self.x_obs[0]
+        weight = 1.0 / self.n_particles
+
+        for i in range(self.n_particles):
+            # TODO: Draw multiple values covering interval from left end to x0
+            tb_sample = self.tbpp.draw_single(x0)
+            particles.append(Particle(tb_sample, [x0], weight))
+
+        return particles
+
+    def forecast(self, obs_index: int):
+        nx_new = 10
+        nx_past = 10
+        xs_new = np.linspace(
+            self.x_obs[obs_index - 1], self.x_obs[obs_index], nx_new
+        ).tolist()
+        delta_x = xs_new[-1] - xs_new[0]
+        x_past_start = self.x_obs[obs_index - 1] - delta_x
+        x_past_end = self.x_obs[obs_index - 1]
+        xs_past = np.linspace(x_past_start, x_past_end, nx_past).tolist()
+
+        for p in self.particles:
+            tb_past = self.tbpp.interpolate_sample(xs_past, p.tb())
+            tb_new = self.tbpp.draw_conditional_points(xs_new, xs_past, tb_past)
+            p.extend(tb_new, xs_new)
+
+    def compute_weights(self, obs_index: int):
+        weights = np.zeros(self.n_particles)
+        for i, p in enumerate(self.particles):
+            weights[i] = self.gr_trend.triplet_likelihood(
+                z_well=self.z_obs[obs_index],
+                z_top=p.z_top[-1],
+                z_base=p.z_base[-1],
+                observed_value=self.gr_obs[obs_index],
+                standard_deviation=self.noise_params["sd_gr"],
+            )
+        weights = weights / np.sum(weights)
+        for i, p in enumerate(self.particles):
+            p.weight = weights[i]
+
+    def resample(self):
+        indices = np.random.choice(
+            range(self.n_particles),
+            size=self.n_particles,
+            p=[p.weight for p in self.particles],
+        )
+        self.particles = [self.particles[i].copy() for i in indices]
